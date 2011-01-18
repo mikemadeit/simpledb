a8ae5c5be886c5b68d0b4e89fb42cfc8b35a878e
Mike <mike@mike-henderson.com>
2011-01-18 17:10:16 -0500

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collect::Application.config.secret_token = 'f5c0a9371f411e6f539e54885e081f447533292906f0ce394e7ec807a1d6fdc4dd1f5984ee334ba45e54663c73bce2666ba49ef3513ed2aa0132e5792c85cfbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Collect::Application.config.session_store :cookie_store, :key => '_collect_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Collect::Application.config.session_store :active_record_store
